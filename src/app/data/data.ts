480fa453bcc79054f7ab62510bfafdb663f04555
Eliezer Palhão <eliezer.ramosp@gmail.com>
2023-09-29 17:37:29 -0300

    PSCA-3 - incluído o arquivo data.ts que contem todos os dados dos games e configurado o componente da pagina home para acessar esses ados e fazer o databind para os demais componentes.

---
+++ src/app/data/data.ts
@@ -0,0 +1,39 @@
+export const data = [
+  {
+    "id":"1",
+    "gameCover":"assets/bt-1.jpg",
+		"gameLabel":"DIGITAL",
+		"gameType": "DIGITAL | PS4",
+		"gamePrice":"R$ 129,99",
+		"gameScore":"5"
+  },
+
+  {
+		"id":"2",
+    "gameCover": "assets/bt-4.jpg",
+		"gameLabel": "EXCLUSIVE",
+		"gameType" : "DISC | PS5",
+		"gamePrice": "R$ 269,99",
+		"gameScore": "5"
+  },
+
+  {
+    "id":"3",
+		"gameCover": "assets/ac-cover.jpg",
+		"gameLabel": "BEST OF YEAR",
+		"gameType" : "DIGITAL | PS5",
+		"gamePrice": "R$ 369,99",
+		"gameScore": "4"
+
+  },
+
+  {
+		"id":"3",
+		"gameCover": "assets/bt-hardline.jpg",
+		"gameLabel": "NEW",
+		"gameType" : "DIGITAL | PS3 PS4 PS5",
+		"gamePrice": "R$ 369,99",
+		"gameScore": "3"
+  },
+
+]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { data } from 'src/app/data/data';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+	gamesList:any[] = data
 
   constructor() { }
 
   ngOnInit(): void {
+		console.log(this.gamesList[0])
+
+		console.log(this.gamesList[1])
   }
 
 }
